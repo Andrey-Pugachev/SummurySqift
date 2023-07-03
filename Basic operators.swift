dedb923e348f4cc0558a7dbb856d8e57e6f3f43d
AndreyPugachev <andreyramenskoe@gmail.com>
2023-07-03 12:10:09 +0300

    add new summary

---
+++ Basic operators.swift
@@ -0,0 +1,52 @@
+/*
+BASIC OPERATORS
+ 
+ Assignment operator (=)
+    В отличии от Си оператор присваивания не возвращает ни какого значения.
+    a = b
+    let (x, y) = (2, 3)
+ 
+ Arithmetic operators (+, -, *, /)
+    В отличие от Си swift не допускает переполнения типа. ?????
+    
+ Remainder operator (%)
+    -9 % 4   // -1
+ 
+ Unary minus operator (-)
+    -54.43
+ 
+ Compound assignment operators (+=, -=, *=, /=)
+    a *= 4
+ 
+ Comparison operators (>, <, >=, <=, ==, !=, ===, !==) ?????
+    По мимо простых значений можно сравнивать кортежи одинаковых типов, но при этом нельзя сравнить булевые значения.
+    (1, "zebra") < (2, "apple")     // true
+    (3, "apple") < (3, "bird")      // true
+    (4, "dog") == (4, "dog")        // true
+ 
+ Ternary conditional operator (? :)
+    a < b ? print("a less then b") : print("a more or equal then b")
+ 
+ Nil-coalescing operator ()
+ ...
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
